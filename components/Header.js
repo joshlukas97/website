d3e3a0bd82ed7e9065eb98128f8454891e2789ef
Josh Lukas <joshlukas97@gmail.com>
2021-03-18 23:18:32 -0500

    Finally added a bio + resume link

--- components/Header.js
+++ components/Header.js
@@ -6,25 +6,22 @@ import faGem from '@fortawesome/fontawesome-free-regular/faGem'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            {/*<span className="icon fa-diamond"></span>*/}
-            {/* <FontAwesomeIcon icon={faGem} transform="grow-18" /> */}
-            {/* <Image></Image> */}
             <img className="portrait" style={{ maxWidth: '100%', height: 'auto', borderRadius: '50%' }} src="../static/images/lake.jpg" alt="me" />
-
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Hi, I'm Josh</h1>
-                <p>I am currently a full stack software engineer at TDS Telecom. I also love rock climbing at Devil's Lake State Park. <br></br>
-                    Feel free to check out my <a href='https://www.instagram.com/joshlukas/'>Instagram</a>.</p>
+                <p>I am a full stack software engineer at TDS Telecom. I also love rock climbing at Devil's Lake State Park. <br></br>
+                    Feel free to check out my <a href='https://www.linkedin.com/in/josh-lukas/'>LinkedIn</a>, <a href='https://www.instagram.com/joshlukas/'>Instagram</a>, or <a 
+                    href='https://www.mountainproject.com/user/111704941/josh-lukas'>Mountain Project</a>.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li> */}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li> */}
+                <li><a href="https://drive.google.com/file/d/1tjf4jqI6PelTQa1VSXQsmypZf3SKILeb/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
+                <li><a href="mailto:joshlukas97@gmail.com">Contact</a></li>
             </ul>
         </nav>
     </header>
